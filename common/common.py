e4a98535b1dfaee78d4be18bba182a4ef76e4f88
wencycool <1023141064@qq.com>
2023-12-10 21:47:44 +0800

    _find_json_strings函数，处理从文本字符串中返回第一个匹配到的json文本

--- common/common.py
+++ common/common.py
@@ -189,6 +189,179 @@ def _find_col_start_stop_pos(header_line: str, col_name: str) -> (int, int):
     else:
         raise ValueError(f"col_name:{col_name}在header_line:{header_line}中找不到起止点")
 
+def _find_json_strings(text):
+    """
+    从文本中查找json文本，并返回第一个匹配到的json文本对象
+
+    注意： 第一个"{"认为是json的开始，对应的配对的"}"认为json的结束，查找到后则返回，如果字符串中存在无效"{"则可能存在解析错误的问题
+
+    解析： tiup cluster display tidb-test  --format=json形式的结果
+    :param text: 传入的字符串
+    :return: 返回第一个匹配到的json格式文本
+    """
+    start = 0
+    stack = []
+    # 只查找第一个json文本
+    first = True
+    for i, char in enumerate(text):
+        if char == '{':
+            stack.append(i)
+            if first:
+                start = i
+                first = False
+        elif char == '}':
+            if stack:
+                stack.pop()
+                if not stack:
+                    end = i + 1
+                    return text[start:end]
+    return ""
 
 if __name__ == "__main__":
-    pass
+    str1 = """
+    tiup is checking updates for component cluster ...
+A new version of cluster is available:
+   The latest version:         v1.14.0
+   Local installed version:    v1.12.5
+   Update current component:   tiup update cluster
+   Update all components:      tiup update --all
+
+Starting component `cluster`: /home/tidb/.tiup/components/cluster/v1.12.5/tiup-cluster display tidb-test --format=json
+{
+  "cluster_meta": {
+    "cluster_type": "tidb",
+    "cluster_name": "tidb-test",
+    "cluster_version": "v7.5.0",
+    "deploy_user": "tidb",
+    "ssh_type": "builtin",
+    "tls_enabled": false,
+    "dashboard_url": "http://192.168.31.201:2379/dashboard",
+    "grafana_urls": [
+      "http://192.168.31.201:3000"
+    ]
+  },
+  "instances": [
+    {
+      "id": "192.168.31.201:9093",
+      "role": "alertmanager",
+      "host": "192.168.31.201",
+      "manage_host": "192.168.31.201",
+      "ports": "9093/9094",
+      "os_arch": "linux/x86_64",
+      "status": "Up",
+      "memory": "-",
+      "memory_limit": "-",
+      "cpu_quota": "-",
+      "since": "-",
+      "data_dir": "/data/tidb-data/alertmanager-9093",
+      "deploy_dir": "/data/tidb-deploy/alertmanager-9093",
+      "ComponentName": "alertmanager",
+      "Port": 9093
+    },
+    {
+      "id": "192.168.31.201:3000",
+      "role": "grafana",
+      "host": "192.168.31.201",
+      "manage_host": "192.168.31.201",
+      "ports": "3000",
+      "os_arch": "linux/x86_64",
+      "status": "Up",
+      "memory": "-",
+      "memory_limit": "-",
+      "cpu_quota": "-",
+      "since": "-",
+      "data_dir": "-",
+      "deploy_dir": "/data/tidb-deploy/grafana-3000",
+      "ComponentName": "grafana",
+      "Port": 3000
+    },
+    {
+      "id": "192.168.31.201:2379",
+      "role": "pd",
+      "host": "192.168.31.201",
+      "manage_host": "192.168.31.201",
+      "ports": "2379/2380",
+      "os_arch": "linux/x86_64",
+      "status": "Up|L|UI",
+      "memory": "-",
+      "memory_limit": "-",
+      "cpu_quota": "-",
+      "since": "-",
+      "data_dir": "/data/tidb-data/pd-2379",
+      "deploy_dir": "/data/tidb-deploy/pd-2379",
+      "ComponentName": "pd",
+      "Port": 2379
+    },
+    {
+      "id": "192.168.31.201:9090",
+      "role": "prometheus",
+      "host": "192.168.31.201",
+      "manage_host": "192.168.31.201",
+      "ports": "9090/12020",
+      "os_arch": "linux/x86_64",
+      "status": "Up",
+      "memory": "-",
+      "memory_limit": "-",
+      "cpu_quota": "-",
+      "since": "-",
+      "data_dir": "/data/tidb-data/prometheus-9090",
+      "deploy_dir": "/data/tidb-deploy/prometheus-9090",
+      "ComponentName": "prometheus",
+      "Port": 9090
+    },
+    {
+      "id": "192.168.31.201:4000",
+      "role": "tidb",
+      "host": "192.168.31.201",
+      "manage_host": "192.168.31.201",
+      "ports": "4000/10080",
+      "os_arch": "linux/x86_64",
+      "status": "Up",
+      "memory": "-",
+      "memory_limit": "-",
+      "cpu_quota": "-",
+      "since": "-",
+      "data_dir": "-",
+      "deploy_dir": "/data/tidb-deploy/tidb-4000",
+      "ComponentName": "tidb",
+      "Port": 4000
+    },
+    {
+      "id": "192.168.31.201:9000",
+      "role": "tiflash",
+      "host": "192.168.31.201",
+      "manage_host": "192.168.31.201",
+      "ports": "9000/8123/3930/20170/20292/8234",
+      "os_arch": "linux/x86_64",
+      "status": "Up",
+      "memory": "-",
+      "memory_limit": "-",
+      "cpu_quota": "-",
+      "since": "-",
+      "data_dir": "/data/tidb-data/tiflash-9000",
+      "deploy_dir": "/data/tidb-deploy/tiflash-9000",
+      "ComponentName": "tiflash",
+      "Port": 9000
+    },
+    {
+      "id": "192.168.31.201:20160",
+      "role": "tikv",
+      "host": "192.168.31.201",
+      "manage_host": "192.168.31.201",
+      "ports": "20160/20180",
+      "os_arch": "linux/x86_64",
+      "status": "Up",
+      "memory": "-",
+      "memory_limit": "-",
+      "cpu_quota": "-",
+      "since": "-",
+      "data_dir": "/data/tidb-data/tikv-20160",
+      "deploy_dir": "/data/tidb-deploy/tikv-20160",
+      "ComponentName": "tikv",
+      "Port": 20160
+    }
+  ]
+}
+{"exit_code":0}
+"""
+    print(_find_json_strings(str1))
