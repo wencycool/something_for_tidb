232d6faebbfb630712cad99cc658b7729a0c4915
wencycool <1023141064@qq.com>
2024-08-10 18:49:41 +0800

    save work

---
+++ ops/checkdb/utils.py
@@ -0,0 +1,16 @@
+# 限制python的内存使用不超过8GB
+
+
+def set_max_memory(max_memory=8 * 1024 * 1024 * 1024):
+    """
+    限制python的内存使用不超过max_memory，异步操作，不会阻塞后续代码执行
+    :param max_memory: 最大内存限制
+    :type max_memory: int
+    """
+    try:
+        import resource
+        resource.setrlimit(resource.RLIMIT_AS, (max_memory, max_memory))
+    except ImportError:
+        return
+
+
